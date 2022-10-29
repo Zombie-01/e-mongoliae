a97759afe69a4aaba0cb5f92aca180a58c52afc5
Zombie-01 <mm6816557@gmail.com>
2022-10-29 17:29:35 +0800

    asd

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [state, setstate] = useState(false);
+  useEffect(() => {
+    const handlechange = () => {
+      setstate(!state);
+    };
+    // window.addEventListener("click", handlechange);
+  }, []);
+  return (
+    <div className="App">
+      <>
+        {state ? (
+          <div className="pre one"></div>
+        ) : (
+          <div className="pre two"></div>
+        )}
+      </>
+    </div>
+  );
+}
+
+export default App;
